4bce8e0bbb7b905d3b7f929a2d36b8374ada0f9e
José Des Sousa <defmoves@pm.me>
2020-01-19 07:24:28 +0000

    converted bot to object

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ const input = fs.readFileSync("input.txt", "utf8").split("\n");
 // Make this immutable?
 const bondaries = input.shift().split("/");
 
+const makeBotObject = botString => {
+  const botArray = botString.split(" ");
+  return {
+    x: botArray[0],
+    y: botArray[1],
+    orientation: botArray[2]
+  };
+};
+
 const parse = input => {
   let bot = "";
   const inputReducer = (accumalator, current) => {
     switch (current.split(" ").length) {
       case 1:
-        return (accumalator += `${bot}: ${current} \n`);
+        const botObject = makeBotObject(bot);
+        return (accumalator += `${JSON.stringify(botObject)}: ${current} \n`);
       case 3:
         bot = current;
         return accumalator;
