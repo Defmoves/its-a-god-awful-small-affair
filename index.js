d647c66a97985a874aaa84e09c590c0df45236e8
José Des Sousa <defmoves@pm.me>
2020-01-19 07:02:13 +0000

    removed boundaries

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const fs = require("fs");
 const input = fs.readFileSync("input.txt", "utf8").split("\n");
+// Make this immutable?
+const bondaries = input.shift().split("/");
 
+console.log(bondaries);
 console.log(input);
