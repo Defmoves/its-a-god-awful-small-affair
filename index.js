b4e444e87426e476711b6bd9014825210324e35f
José Des Sousa <defmoves@pm.me>
2020-01-19 07:15:59 +0000

    reduced input tp bot + commands

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const input = fs.readFileSync("input.txt", "utf8").split("\n");
 const bondaries = input.shift().split("/");
 
 const parse = input => {
-  return input;
+  let bot = "";
+  const inputReducer = (accumalator, current) => {
+    switch (current.split(" ").length) {
+      case 1:
+        return (accumalator += `${bot}: ${current} \n`);
+      case 3:
+        bot = current;
+        return accumalator;
+    }
+  };
+
+  return input.reduce(inputReducer, "");
 };
 
 const output = parse(input);
